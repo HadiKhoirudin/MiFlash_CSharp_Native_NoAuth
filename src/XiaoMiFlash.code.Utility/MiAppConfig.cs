eae30d552bba78607a75045ab327501f8b20792c
HadiKhoirudin <347extreme@gmail.com>
2025-07-20 17:38:16 +0700

    Fix Bugs at PATCH Operations

--- src/XiaoMiFlash.code.Utility/MiAppConfig.cs
+++ src/XiaoMiFlash.code.Utility/MiAppConfig.cs
@@ -9,9 +9,9 @@ public class MiAppConfig
 {
 	public static void Add(string key, string value)
 	{
-		Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-		configuration.AppSettings.Settings.Add(key, value);
-		configuration.Save();
+		//Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+		//configuration.AppSettings.Settings.Add(key, value);
+		//configuration.Save();
 	}
 
 	public static void SetValue(string AppKey, string AppValue)
@@ -26,26 +26,26 @@ public class MiAppConfig
 
 	public static string GetAppConfig(string appKey)
 	{
-		XmlDocument xmlDocument = new XmlDocument();
-		xmlDocument.Load(Application.ExecutablePath + ".config");
-		XmlElement xmlElement = (XmlElement)xmlDocument.SelectSingleNode("//appSettings").SelectSingleNode("//add[@key='" + appKey + "']");
-		if (xmlElement != null)
-		{
-			return xmlElement.Attributes["value"].Value;
-		}
+		//XmlDocument xmlDocument = new XmlDocument();
+		//xmlDocument.Load(Application.ExecutablePath + ".config");
+		//XmlElement xmlElement = (XmlElement)xmlDocument.SelectSingleNode("//appSettings").SelectSingleNode("//add[@key='" + appKey + "']");
+		//if (xmlElement != null)
+		//{
+		//	return xmlElement.Attributes["value"].Value;
+		//}
 		return string.Empty;
 	}
 
 	public static string Get(string key)
 	{
-		if (ConfigurationManager.AppSettings[key] == null)
-		{
-			Add(key, "");
-		}
-		if (ConfigurationManager.AppSettings[key] != null)
-		{
-			return ConfigurationManager.AppSettings[key].ToString();
-		}
+		//if (ConfigurationManager.AppSettings[key] == null)
+		//{
+		//	Add(key, "");
+		//}
+		//if (ConfigurationManager.AppSettings[key] != null)
+		//{
+		//	return ConfigurationManager.AppSettings[key].ToString();
+		//}
 		return "";
 	}
 }
